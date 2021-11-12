a294fd427bad5806326685fd78656f735b2e2693
Dahud Yusuf Ishola <abiodunyusuf4@gmail.com>
2021-11-12 12:59:03 +0100

    single price grid component

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primarycyan: 'hsl(179, 62%, 43%)',
+        primaryyellow:'hsl(71, 73%, 54%)',
+        primarygray: 'hsl(204, 43%, 93%)', 
+        primarygrayishblue: 'hsl(218, 22%, 67%)',
+        secondarycyan: 'rgba(42, 178, 176, 0.7)'
+      },
+      
+      body: ['karla']
+    
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
